37f86403eaf2a021d8bd7f8ad5fd55d2be791389
Pliffdax <stepanov-oleksandr@ukr.net>
2025-12-08 12:30:00 +0200

    perf: improve prisma logging

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,11 +1,24 @@
 import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient, Prisma } from '@prisma/client';
 
 @Injectable()
 export class PrismaService
-  extends PrismaClient
+  extends PrismaClient<Prisma.PrismaClientOptions, 'query' | 'error'>
   implements OnModuleInit, OnModuleDestroy
 {
+  constructor() {
+    super({
+      log: [
+        { emit: 'event', level: 'query' },
+        { emit: 'event', level: 'error' },
+      ],
+    });
+
+    this.$on('query', (e: Prisma.QueryEvent) => {
+      console.log(`Query: ${e.query} | Duration: ${e.duration}ms`);
+    });
+  }
+
   async onModuleInit() {
     await this.$connect();
   }
